c8d50aece26d492f3e1e9637285f6243b5f47039
Ahmed-Reda-elshahawy <ahmed92.5elshahawy@gmail.com>
2023-08-30 09:27:06 +0300

    update some designs

--- src/components/content/ListOfCards.jsx
+++ src/components/content/ListOfCards.jsx
@@ -3,12 +3,12 @@ import { CardsData } from "../../Data/CardsData";
 import ProductCard from './ProductCard';
 
 function ListOfCards() {
-    const [Products, setProducts] = useState(CardsData)
-    console.log(Products.length);
+    // const [Products, setProducts] = useState(CardsData)
+    // console.log(Products.length);
 
     return (
         <div className='container mt-5 mb-5 pt-4 pb-4 d-flex flex-wrap gap-3 justify-content-center'>
-            {Products.map(card => {
+            {CardsData.map(card => {
                 return (
                     <div key={card.id}>
                         <ProductCard data={card} />
--- src/components/content/ProductCard.jsx
+++ src/components/content/ProductCard.jsx
@@ -1,31 +1,37 @@
-import React, { useState } from 'react';
-import { Button, Card, Modal } from 'react-bootstrap';
+import React from 'react';
+import { Card } from 'react-bootstrap';
 import "../../style/cards.css";
-import Popup from '../Popup';
+import { useNavigate } from 'react-router-dom';
+// import Popup from '../Popup';
 
 
 function ProductCard(props) {
-    let [cards, setCards] = useState(0);
+    // let [cards, setCards] = useState(0);
 
-    let AddProduct = () => {
-        setCards(cards += 1);
+    // let AddProduct = () => {
+    //     setCards(cards += 1);
+    // }
+    // let navigate = useNavigate();
+    let handelDetails = () => {
+        console.log(props.data.id);
+        // navigate(`/details/${props.data.id}`);
     }
 
 
     return (
-        <Card style={{ width: '19rem' }}>
+        <Card style={{ width: '20rem' }} onClick={handelDetails}>
             <div className="img-container">
                 <Card.Img className='card-img w-100' variant="top" src={`/Images/${props.data.image}`} />
             </div>
-            <Card.Body>
+            <Card.Body className='d-flex justify-content-between'>
                 <Card.Title>{props.data.title}</Card.Title>
-                <Card.Text className='text-bold'>
+                <Card.Text className='text-bold text-warning'>
                     ${props.data.price}
                 </Card.Text>
-                <div className="btns d-flex justify-content-between">
+                {/* <div className="btns d-flex justify-content-between">
                     <Popup cardData={props.data} />
                     <Button variant="warning" onClick={AddProduct}>+</Button>
-                </div>
+                </div> */}
             </Card.Body>
         </Card>
     )
