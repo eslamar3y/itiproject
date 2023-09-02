30ebbf9fe09c3f5569c15acf1c49817eacb544f6
eslam <emaree86@gmail.com>
2023-09-02 11:10:52 +0300

    add contact us and about us pages

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
-
+import React from "react";
+import "../style/aboutus.css";
 function AboutSection() {
-    return (
-        <div className='min-vh-100'>AboutSection</div>
-    )
+  return (
+    <>
+      <div className="min-vh-100">
+        <div className="imgabout">
+          <div className="secdiv">
+            <h1>About Us</h1>
+            <h4>Welcome To Our Website </h4>
+            <h6>Good Food Starts With Good Ingedients .</h6>
+            <p>
+              {" "}
+              As a heart-centered brand that cares about its global impct,
+              <br></br>pizza website emphasizes that key part of its brand in
+              its mission statement .<br></br> Describin your brand values and
+              what you stand for is<br></br> one of the best ways to connect
+              with your audince
+            </p>
+          </div>
+        </div>
+
+        <div className="animate">
+          <h1>One Thousand Flavors in One Place</h1>
+        </div>
+      </div>
+
+      <div className="thdiv">
+        <div className="thdiv-div">
+          <h6>MESAAGE FROM FOUNDER</h6>
+          <h1>First, We eat. Then,We do everything else. </h1>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default AboutSection;
--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
-
+import React from "react";
+import "../style/contactus.css";
 function ContactSection() {
-    return (
-        <div className='min-vh-100'>ContactSection</div>
-    )
+  return (
+    <div className="main min-vh-100 ">
+      <div className="contain">
+        <div className="form">
+          <h1>Get in Touch</h1>
+          <p>Feel Free to drop us a line below!</p>
+
+          <input
+            type="text"
+            className="input inputt"
+            required
+            placeholder="Your Name"
+          ></input>
+          <br></br>
+          <input
+            type="email"
+            className="input"
+            required
+            placeholder="Your Email"
+          ></input>
+          <br></br>
+          <input
+            type="textbox"
+            className="input inputtext"
+            required
+            placeholder="Typing Your message here...."
+          ></input>
+          <br></br>
+
+          <input type="submit" value="send" className="button"></input>
+        </div>
+        <div className="contact">
+          <div className="seccontain">
+            <h1>Contact Us</h1>
+
+            <span>
+              {" "}
+              <i class="fa-solid fa-location-dot"></i>32,Mansoura 32524 NewCity
+            </span>
+            <br></br>
+            <span>
+              <i class="fa-solid fa-envelope"></i>maro@yahoo.com
+            </span>
+            <br></br>
+
+            <span>
+              <i class="fa-solid fa-phone"></i>+2765 7654 2105
+            </span>
+            <br></br>
+            <span>
+              {" "}
+              <i class="fa-solid fa-print"></i>+4536 8732 5432
+            </span>
+            <br></br>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ContactSection
+export default ContactSection;
